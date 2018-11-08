05abc8ef38dd4c67e2ce505bd7d72fb50330ac41
pini-girit <pini@girit.biz>
2018-11-08 13:34:41 +0200

    CLOUDINARY-2: Added support for product videos from Cloudinary (on admin & on product page)

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            loadPlayer: 'Cloudinary_Cloudinary/js/load-player',
+            'Magento_ProductVideo/js/fotorama-add-video-events': 'Cloudinary_Cloudinary/js/fotorama-add-video-events'
+        }
+    },
+};
