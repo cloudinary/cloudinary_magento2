1c968d0caebc2f35259a32afd318ecab49df4041
pini-girit <pini@girit.biz>
2018-12-26 10:57:22 +0200

    Fixed M2.3 issues: Product page image not loading from Cloudinary + custom transformations not working on category & product

--- Plugin/Catalog/Model/Product/Image/UrlBuilder.php
+++ Plugin/Catalog/Model/Product/Image/UrlBuilder.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Cloudinary\Cloudinary\Plugin\Catalog\Block\Product;
+namespace Cloudinary\Cloudinary\Plugin\Catalog\Model\Product\Image;
 
 use Cloudinary\Cloudinary\Core\ConfigurationInterface;
 use Cloudinary\Cloudinary\Core\Image\ImageFactory as CloudinaryImageFactory;
@@ -13,14 +13,12 @@ use Cloudinary\Cloudinary\Core\UrlGenerator;
 use Cloudinary\Cloudinary\Model\Transformation as TransformationModel;
 use Cloudinary\Cloudinary\Model\TransformationFactory;
 use Magento\Catalog\Api\Data\ProductInterface;
-use Magento\Catalog\Block\Product\Image as ImageBlock;
-use Magento\Catalog\Block\Product\ImageFactory as CatalogImageFactory;
 use Magento\Catalog\Helper\Image as CatalogImageHelper;
-use Magento\Catalog\Model\Product;
 use Magento\Catalog\Model\Product\Image\ParamsBuilder;
+use Magento\Catalog\Model\Product\Image\UrlBuilder as CatalogUrlBuilder;
 use Magento\Framework\View\ConfigInterface;
 
-class ImageFactory
+class UrlBuilder
 {
     /**
      * @var ConfigInterface
@@ -100,37 +98,37 @@ class ImageFactory
     }
 
     /**
-     * Create image block from product
-     * @param CatalogImageFactory $catalogImageFactory
+     * Build image url using base path and params
+     *
+     * @param CatalogUrlBuilder $catalogUrlBuilder
      * @param callable $proceed
-     * @param Product $product
-     * @param string $imageId
-     * @param array|null $attributes
-     * @return ImageBlock
+     * @param string $baseFilePath
+     * @param string $imageDisplayArea
+     * @return string
      */
-    public function aroundCreate(CatalogImageFactory $catalogImageFactory, callable $proceed, Product $product, string $imageId, array $attributes = null): ImageBlock
+    public function aroundGetUrl(CatalogUrlBuilder $catalogUrlBuilder, callable $proceed, string $baseFilePath, string $imageDisplayArea): string
     {
-        $imageBlock = $proceed($product, $imageId, $attributes);
+        $url = $proceed($baseFilePath, $imageDisplayArea);
         if (!$this->configuration->isEnabled()) {
-            return $imageBlock;
+            return $url;
         }
 
         try {
-            if (strpos($imageBlock->getImageUrl(), $this->configuration->getMediaBaseUrl() . 'catalog/product') === 0) {
-                $viewImageConfig = $this->presentationConfig->getViewConfig()->getMediaAttributes(
+            if (strpos($url, $this->configuration->getMediaBaseUrl() . 'catalog/product') === 0) {
+                $imageArguments = $this->presentationConfig->getViewConfig()->getMediaAttributes(
                     'Magento_Catalog',
                     CatalogImageHelper::MEDIA_TYPE_CONFIG_NODE,
-                    $imageId
+                    $imageDisplayArea
                 );
-                $imageMiscParams = $this->imageParamsBuilder->build($viewImageConfig);
+                $imageMiscParams = $this->imageParamsBuilder->build($imageArguments);
 
-                $imagePath = preg_replace('/^' . preg_quote($this->configuration->getMediaBaseUrl(), '/') . '/', '/', $imageBlock->getImageUrl());
+                $imagePath = preg_replace('/^' . preg_quote($this->configuration->getMediaBaseUrl(), '/') . '/', '/', $url);
                 $imagePath = preg_replace('/\/catalog\/product\/cache\/[a-f0-9]{32}\//', '/', $imagePath);
 
                 $image = $this->cloudinaryImageFactory->build(
                     sprintf('catalog/product%s', $imagePath),
-                    function () use ($imageBlock) {
-                        return $imageBlock->getImageUrl();
+                    function () use ($url) {
+                        return $url;
                     }
                 );
 
@@ -141,14 +139,14 @@ class ImageFactory
                         $imagePath
                     )
                 );
-                $imageBlock->setOriginalImageUrl($imageBlock->setImageUrl());
-                $imageBlock->setImageUrl($generatedImageUrl);
+
+                $url = $generatedImageUrl;
             }
         } catch (\Exception $e) {
-            $imageBlock = $proceed($product, $imageId, $attributes);
+            $url = $proceed($baseFilePath, $imageDisplayArea);
         }
 
-        return $imageBlock;
+        return $url;
     }
 
     /**
