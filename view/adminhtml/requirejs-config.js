a3139fd171694ece45083b89937f0b326cf1c62a
pini-girit <pini@girit.biz>
2019-01-13 17:16:20 +0200

    Working on Media Library implementation - product edit page in progress

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -3,7 +3,13 @@ var config = {
         '*': {
             cloudinaryFreeTransform: 'Cloudinary_Cloudinary/js/cloudinary-free',
             newVideoDialog: 'Cloudinary_Cloudinary/js/new-video-dialog',
-            'Magento_ProductVideo/js/get-video-information': 'Cloudinary_Cloudinary/js/get-video-information'
+            'Magento_ProductVideo/js/get-video-information': 'Cloudinary_Cloudinary/js/get-video-information',
+            selectImageFromCloudinaryDialog: 'Cloudinary_Cloudinary/js/new-image-from-cloudinary-dialog',
+            cloudinaryMediaLibraryModal: 'Cloudinary_Cloudinary/js/cloudinary-media-library-modal'
         }
     },
+    paths: {
+        cloudinaryMediaLibraryAll: "//media-library.cloudinary.com/global/all",
+        es6Promise: "//cdnjs.cloudflare.com/ajax/libs/es6-promise/4.1.1/es6-promise.auto.min"
+    }
 };
