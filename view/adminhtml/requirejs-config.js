7aa7a9d72e280f4d7c823d98a79ec793ca9f5d00
pini-girit <pini@girit.biz>
2019-01-17 10:24:40 +0200

    Implementing Cloudinary Media Library (in progress) - currently available on product gallery edit only with images

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -4,7 +4,6 @@ var config = {
             cloudinaryFreeTransform: 'Cloudinary_Cloudinary/js/cloudinary-free',
             newVideoDialog: 'Cloudinary_Cloudinary/js/new-video-dialog',
             'Magento_ProductVideo/js/get-video-information': 'Cloudinary_Cloudinary/js/get-video-information',
-            selectImageFromCloudinaryDialog: 'Cloudinary_Cloudinary/js/new-image-from-cloudinary-dialog',
             cloudinaryMediaLibraryModal: 'Cloudinary_Cloudinary/js/cloudinary-media-library-modal'
         }
     },
