84d29094b58a007417a80409ce3d71f5f0131963
pini-girit <pini@girit.biz>
2019-02-05 11:06:34 +0200

    CLOUDINARY-74,CLOUDINARY-84: Reversed migration CLI

--- Command/DownloadImages.php
+++ Command/DownloadImages.php
@@ -6,10 +6,22 @@ use Cloudinary\Cloudinary\Model\BatchDownloader;
 use Cloudinary\Cloudinary\Model\Logger\OutputLogger;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class DownloadImages extends Command
 {
+    /**#@+
+     * Keys and shortcuts for input arguments and options
+     */
+    const OVERRIDE = 'override';
+    /**#@- */
+
+    const OVERRIDE_CONFIRM_MESSAGE = "<question>Are you sure you want to override local files (y/n)[n]?</question>";
+
+    private $_override = false;
+
     /**
      * @var BatchDownloader
      */
@@ -40,6 +52,14 @@ class DownloadImages extends Command
     {
         $this->setName('cloudinary:download:all');
         $this->setDescription('Download images from Cloudinary to the local pub/media dir');
+        $this->setDefinition([
+            new InputOption(
+                self::OVERRIDE,
+                '-o',
+                InputOption::VALUE_NONE,
+                'Override local images if already exists'
+            ),
+        ]);
     }
 
     /**
@@ -51,10 +71,26 @@ class DownloadImages extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         try {
+            if ($input->getOption(self::OVERRIDE) && $this->confirmQuestion(self::OVERRIDE_CONFIRM_MESSAGE, $input, $output)) {
+                $this->_override = true;
+            }
             $this->outputLogger->setOutput($output);
-            $this->batchDownloader->downloadUnsynchronisedImages($this->outputLogger);
+            $this->batchDownloader->downloadUnsynchronisedImages($this->outputLogger, $this->_override);
         } catch (\Exception $e) {
             $output->writeln($e->getMessage());
         }
+    }
+
+    /**
+     * @method confirmQuestion
+     * @param string $message
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return bool
+     */
+    private function confirmQuestion(string $message, InputInterface $input, OutputInterface $output)
+    {
+        $confirmationQuestion = new ConfirmationQuestion($message, false);
+        return (bool)$this->getHelper('question')->ask($input, $output, $confirmationQuestion);
     }
 }
