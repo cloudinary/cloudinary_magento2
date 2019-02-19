26dc635fc47811592c97ce6306c54c8d89e0f6e6
pini-girit <pini@girit.biz>
2019-02-19 18:04:58 +0200

    CLOUDINARY-74: Refactored ML Wysiwyg imageUploaderUrl in order to remove the use of  & download remote URL instead...

--- Helper/MediaLibraryHelper.php
+++ Helper/MediaLibraryHelper.php
@@ -4,8 +4,6 @@ namespace Cloudinary\Cloudinary\Helper;
 
 use Cloudinary\Cloudinary\Core\ConfigurationInterface;
 use Magento\Framework\App\Helper\Context;
-use Magento\Framework\App\RequestInterface;
-use Magento\Framework\HTTP\Client\Curl;
 
 class MediaLibraryHelper extends \Magento\Framework\App\Helper\AbstractHelper
 {
@@ -13,16 +11,6 @@ class MediaLibraryHelper extends \Magento\Framework\App\Helper\AbstractHelper
      * @var ConfigurationInterface
      */
     protected $configuration;
-
-    /**
-     * @var RequestInterface
-     */
-    protected $request;
-
-    /**
-     * @var Curl
-     */
-    protected $curl;
 
     /**
      * Cloudinary credentials
@@ -51,24 +39,13 @@ class MediaLibraryHelper extends \Magento\Framework\App\Helper\AbstractHelper
     /**
      * @param Context $context
      * @param ConfigurationInterface $configuration
-     * @param RequestInterface $request
-     * @param Curl $curl
      */
     public function __construct(
         Context $context,
-        ConfigurationInterface $configuration,
-        RequestInterface $request,
-        Curl $curl
+        ConfigurationInterface $configuration
     ) {
         parent::__construct($context);
         $this->configuration = $configuration;
-        $this->request = $request;
-        $this->curl = $curl;
-    }
-
-    protected function getRequest()
-    {
-        return $this->request;
     }
 
     /**
@@ -130,29 +107,5 @@ class MediaLibraryHelper extends \Magento\Framework\App\Helper\AbstractHelper
             ];
         }
         return $options;
-    }
-
-    public function convertRequestAssetUrlToImage()
-    {
-        if ($this->configuration->isEnabled()) {
-            $asset = $this->getRequest()->getPostValue("asset");
-            $this->curl->get($asset['secure_url']);
-            $asset['image'] = $this->curl->getBody();
-            $this->getRequest()->setPostValue("asset", $asset);
-
-            $tmpfile = tmpfile();
-            fwrite($tmpfile, $asset['image']);
-            chmod(stream_get_meta_data($tmpfile)['uri'], 0644);
-
-            $_FILES[$this->getRequest()->getParam('param_name', 'image')] = [
-                "name" => basename($asset['url']),
-                "type" => "{$asset['resource_type']}/{$asset['format']}",
-                "tmp_name" => stream_get_meta_data($tmpfile)['uri'],
-                "error" => 0,
-                "size" => $asset['bytes'],
-            ];
-
-            return $tmpfile;
-        }
     }
 }
