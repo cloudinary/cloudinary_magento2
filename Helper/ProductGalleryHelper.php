ea2ec05c2fe47f6c7533346b78bf2127bf14b9b7
pini-girit <pini@girit.biz>
2019-03-14 09:52:03 +0200

    CLOUDINARY-102: Working on Cloudinary-Product-Gallery implementation

--- Helper/ProductGalleryHelper.php
+++ Helper/ProductGalleryHelper.php
@@ -34,21 +34,23 @@ class ProductGalleryHelper extends \Magento\Framework\App\Helper\AbstractHelper
     /**
      * @method getCloudinaryPGOptions
      * @param bool $refresh Refresh options
+     * @param bool $ignoreDisabled Get te options even if the module or the product gallery are disabled
      * @return array
      */
-    public function getCloudinaryPGOptions($refresh = true)
+    public function getCloudinaryPGOptions($refresh = false, $ignoreDisabled = false)
     {
-        if ((is_null($this->cloudinaryMLoptions) || $refresh) && $this->configuration->isEnabled() && $this->configuration->isEnabledProductGallery()) {
+        if ((is_null($this->cloudinaryPGoptions) || $refresh) && ($ignoreDisabled || ($this->configuration->isEnabled() && $this->configuration->isEnabledProductGallery()))) {
             $this->cloudinaryPGoptions = $this->configuration->getProductGalleryAll();
             foreach ($this->cloudinaryPGoptions as $key => $value) {
                 $path = explode("_", $key);
-                if (in_array($path[0], ['themeProps','zoomProps','thumbnailProps','indicatorProps'])) {
-                    if (!isset($this->cloudinaryPGoptions[$path[0]])) {
-                        $this->cloudinaryPGoptions[$path[0]] = [];
+                $_path = $path[0];
+                if (in_array($_path, ['themeProps','zoomProps','thumbnailProps','indicatorProps'])) {
+                    if (!isset($this->cloudinaryPGoptions[$_path])) {
+                        $this->cloudinaryPGoptions[$_path] = [];
                     }
                     array_shift($path);
                     $path = implode("_", $path);
-                    $this->cloudinaryPGoptions[$path[0]][$path] = $value;
+                    $this->cloudinaryPGoptions[$_path][$path] = $value;
                     unset($this->cloudinaryPGoptions[$key]);
                 }
             }
@@ -62,6 +64,14 @@ class ProductGalleryHelper extends \Magento\Framework\App\Helper\AbstractHelper
             }
         }
 
-        return $this->cloudinaryMLoptions;
+        return $this->cloudinaryPGoptions;
+    }
+
+    /**
+     * @return bool
+     */
+    public function canDisplayProductGallery()
+    {
+        return ($this->configuration->isEnabled() && $this->configuration->isEnabledProductGallery()) ? true : false;
     }
 }
