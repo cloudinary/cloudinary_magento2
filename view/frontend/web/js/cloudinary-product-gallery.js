d2f59c4e0a424b14d4ba1f9900beaeb62bf5fbce
pini-girit <pini@girit.biz>
2019-03-14 14:48:56 +0200

    CLOUDINARY-102: Working on Cloudinary-Product-Gallery implementation

---
+++ view/frontend/web/js/cloudinary-product-gallery.js
@@ -0,0 +1,35 @@
+define([
+    'jquery',
+    'cloudinaryProductGalleryAll'
+], function($) {
+    'use strict';
+
+    $.widget('mage.cloudinaryProductGallery', {
+
+        options: {
+            cloudinaryPGoptions: {}, // Options for Cloudinary-PG galleryWidget()
+            cldPGid: 0,
+        },
+
+        /**
+         * @private
+         */
+        _create: function() {
+            this._super();
+
+            console.log(this.options.cloudinaryPGoptions);
+            var widget = this;
+            window.cloudinary_pg = window.cloudinary_pg || [];
+            this.options.cldPGid = this.options.cldPGid || 0;
+            if (typeof window.cloudinary_pg[this.options.cldPGid] === "undefined") {
+                this.cloudinary_pg = window.cloudinary_pg[this.options.cldPGid] = cloudinary.galleryWidget(this.options.cloudinaryPGoptions);
+                this.cloudinary_pg.render();
+            } else {
+                this.cloudinary_pg = window.cloudinary_pg[this.options.cldPGid];
+            }
+        },
+
+    });
+
+    return $.mage.cloudinaryProductGallery;
+});
