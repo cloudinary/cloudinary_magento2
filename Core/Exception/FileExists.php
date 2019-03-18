a20b877934720b3e000429482c928c7fce8450b6
pini-girit <pini@girit.biz>
2019-03-18 10:40:03 +0200

    CLOUDINARY-109: Disabled exception throwing the FileExists exception on backend saves

--- Core/Exception/FileExists.php
+++ Core/Exception/FileExists.php
@@ -4,5 +4,5 @@ namespace Cloudinary\Cloudinary\Core\Exception;
 
 class FileExists extends MigrationError
 {
-    const DEFAULT_MESSAGE = 'File already exists (cloudinary is case insensitive!!).';
+    const DEFAULT_MESSAGE = 'File already exists (cloudinary is case insensitive).';
 }
