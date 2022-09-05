7cc409f278464f77a1410a03b86320c71ecd5ee5
Ariel Ashri <arieliens@gmail.com>
2022-09-05 15:26:37 +0300

    Cloudinary 1.19.0

--- Core/Security/CloudinaryEnvironmentVariable.php
+++ Core/Security/CloudinaryEnvironmentVariable.php
@@ -2,42 +2,76 @@
 
 namespace Cloudinary\Cloudinary\Core\Security;
 
-use Cloudinary;
-use Cloudinary\Cloudinary\Core\Cloud;
+use Cloudinary\Cloudinary;
 use Cloudinary\Cloudinary\Core\Credentials;
+use Cloudinary\Configuration\Configuration;
 
 class CloudinaryEnvironmentVariable implements EnvironmentVariable
 {
+    /**
+     * @var array|string|string[]
+     */
     private $environmentVariable;
 
+    /**
+     * @var $configuration
+     */
+    private $configuration;
+
+    /**
+     * @param $environmentVariable
+     * @throws Cloudinary\Core\Exception\InvalidCredentials
+     */
     private function __construct($environmentVariable)
     {
         $this->environmentVariable = (string)$environmentVariable;
         try {
-            Cloudinary::config_from_url(str_replace('CLOUDINARY_URL=', '', $environmentVariable));
+            //Cloudinary::config_from_url(str_replace('CLOUDINARY_URL=', '', $environmentVariable));
+            $this->environmentVariable = str_replace('CLOUDINARY_URL=', '', $environmentVariable);
         } catch (\Exception $e) {
             throw new \Cloudinary\Cloudinary\Core\Exception\InvalidCredentials('Cloudinary config creation from environment variable failed');
         }
     }
 
+    /**
+     * @param $environmentVariable
+     * @return CloudinaryEnvironmentVariable
+     * @throws Cloudinary\Core\Exception\InvalidCredentials
+     */
     public static function fromString($environmentVariable)
     {
         return new CloudinaryEnvironmentVariable($environmentVariable);
     }
 
+    /**
+     * @return string
+     */
     public function getCloud()
     {
-        return Cloud::fromName(Cloudinary::config_get('cloud_name'));
+        if (!$this->configuration) {
+            $this->configuration = new Configuration($this->environmentVariable);
+        }
+        return $this->configuration->cloud->cloudName;
     }
 
+    /**
+     * @return Credentials
+     */
     public function getCredentials()
     {
+        if (!$this->configuration) {
+            $this->configuration = new Configuration($this->environmentVariable);
+        }
+
         return Credentials::fromKeyAndSecret(
-            Key::fromString(Cloudinary::config_get('api_key')),
-            Secret::fromString(Cloudinary::config_get('api_secret'))
+            Key::fromString($this->configuration->cloud->apiKey),
+            Secret::fromString($this->configuration->cloud->apiSecret)
         );
     }
 
+    /**
+     * @return array|string|string[]
+     */
     public function __toString()
     {
         return $this->environmentVariable;
