278dd9006f802f4e92c8de5c9b3a64fda6628f75
Ariel Ashri <arieliens@gmail.com>
2023-06-12 12:23:50 +0300

    CLOUDINARY-450 - fix enable / disable issue

--- Core/ConfigurationBuilder.php
+++ Core/ConfigurationBuilder.php
@@ -23,25 +23,27 @@ class ConfigurationBuilder
 
         $reg = $this->configuration->getCoreRegistry();
         $credentials = $this->configuration->getCredentials();
+        if ($credentials) {
+            $cloud = [
+                'cloud_name' => $credentials['cloud_name'],
+                'api_key' =>  $credentials['api_key'],
+                'api_secret' => $credentials['api_secret']
+            ];
 
-        $cloud = [
-            'cloud_name' => $credentials['cloud_name'],
-            'api_key' =>  $credentials['api_key'],
-            'api_secret' => $credentials['api_secret']
-        ];
+            $url = array_diff($credentials, $cloud);
 
-        $url = array_diff($credentials, $cloud);
+            $config = array('cloud' => $cloud);
 
-        $config = array('cloud' => $cloud);
+            if ($url && is_array($url)) {
+                $config['url'] = $url;
+            }
 
-        if ($url && is_array($url)) {
-            $config['url'] = $url;
+            if ($this->configuration->getCdnSubdomainStatus()) {
+                $config['cloud']['cdn_subdomain'] = true;
+            }
+
+            return $config;
         }
-
-        if ($this->configuration->getCdnSubdomainStatus()) {
-            $config['cloud']['cdn_subdomain'] = true;
-        }
-
-        return $config;
+        return $this;
     }
 }
