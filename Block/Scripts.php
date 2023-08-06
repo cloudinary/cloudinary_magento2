ac781cb6cba63c5a443fad9909bd1287666c4a13
Adam Rahwane <adam.rahwane@gmail.com>
2023-08-06 20:53:05 +0300

    fix: credentials accessed before they exist by the scripts block

--- Block/Scripts.php
+++ Block/Scripts.php
@@ -2,31 +2,56 @@
 
 namespace Cloudinary\Cloudinary\Block;
 
+use Cloudinary\Cloudinary\Core\ConfigurationInterface;
 use Cloudinary\Cloudinary\Helper\MediaLibraryHelper;
 use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
 
 class Scripts extends Template
 {
+    /**
+     * @var ConfigurationInterface
+     */
+    private $configuration;
 
+    /**
+     * @var MediaLibraryHelper
+     */
     protected $_helper;
 
+    /**
+     * @param Context $context
+     * @param ConfigurationInterface $configuration
+     * @param MediaLibraryHelper $mediaHelper
+     * @param array $data
+     */
     public function __construct(
-        Template\Context $context,
+        Context $context,
+        ConfigurationInterface $configuration,
         MediaLibraryHelper $mediaHelper,
         array $data = []
     )
     {
-        parent::__construct($context, $data);
+        $this->configuration = $configuration;
         $this->_helper = $mediaHelper;
+        parent::__construct($context, $data);
     }
 
     /**
-     * @return void
+     * @method isEnabledLazyload
+     * @return boolean
+     */
+    public function isEnabled()
+    {
+        return $this->configuration->isEnabled();
+    }
+
+    /**
+     * @return string|null
      */
     public function getCname()
     {
         return $this->_helper->getCname();
     }
-
 
 }
