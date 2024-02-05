7198a099934fe4951da6efbf77e79cfe973444ec
Ariel Ashri <arieliens@gmail.com>
2024-02-05 11:48:59 +0200

    fix cld videosettings case when cld in disabled

--- Block/VideoSettings.php
+++ Block/VideoSettings.php
@@ -28,24 +28,29 @@ class VideoSettings extends template
      * @return array
      */
     public function getVideoSettings() {
-        $allSettings = $this->configuration->getAllVideoSettings();
-
-        $autoplay = (isset($allSettings['autoplay']) && $allSettings['autoplay'] != "never");
-        $controls = (isset($allSettings['controls']) && $allSettings['controls'] == "all");
-        $playerSettings = [
-            "cloudName" => $this->configuration->getCloud(),
-            'controls' => $controls,
-            'autoplay' => $autoplay,
-            'loop' => (bool) $allSettings['loop'],
-            'muted' => (bool) $allSettings['sound']
-        ];
-        if (isset($allSettings['use_abr']) && $allSettings['use_abr'] != "none") {
-            $playerSettings['sourceTypes'] = [$allSettings['use_abr']];
-        }
         $settings = [
-            'player_type' => $allSettings['player_type'],
-            'settings' => $playerSettings
+            'player_type' => 'default'
         ];
+        if ($this->configuration->isEnabled()) {
+            $allSettings = $this->configuration->getAllVideoSettings();
+            $autoplay = (isset($allSettings['autoplay']) && $allSettings['autoplay'] != "never");
+            $controls = (isset($allSettings['controls']) && $allSettings['controls'] == "all");
+            $playerSettings = [
+                "cloudName" => $this->configuration->getCloud(),
+                'controls' => $controls,
+                'autoplay' => $autoplay,
+                'loop' => (bool) $allSettings['loop'],
+                'muted' => (bool) $allSettings['sound']
+            ];
+            if (isset($allSettings['use_abr']) && $allSettings['use_abr'] != "none") {
+                $playerSettings['sourceTypes'] = [$allSettings['use_abr']];
+            }
+            $settings = [
+                'player_type' => $allSettings['player_type'],
+                'settings' => $playerSettings
+            ];
+        }
+
 
         return $settings;
     }
