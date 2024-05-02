7b6a6959510a47e619564b8c89ab369507556d73
Ariel Ashri <arieliens@gmail.com>
2024-05-02 18:04:13 +0300

    fix bug on VideoSettings.php (adams request) - p2

--- Block/VideoSettings.php
+++ Block/VideoSettings.php
@@ -32,30 +32,32 @@ class VideoSettings extends template
             'player_type' => 'default'
         ];
         $allSettings = $this->configuration->getAllVideoSettings();
-
+        $videoFreeParamsArray = [];
+        $sourceTypes = null;
         $videoPlayerEnabled = isset($allSettings['enabled']) && (bool)$allSettings['enabled'];
         $videoFreeParams = isset($allSettings['video_free_params']) ? $allSettings['video_free_params'] : null;
-        $videoFreeParamsArray = json_decode($videoFreeParams, true);
-        $sourceTypes = null;
+        if (!empty($videoFreeParams) && $videoFreeParams != "[]") {
+            $videoFreeParamsArray = json_decode($videoFreeParams, true);
+            $videoFreeParamsArray['player']['cloudName'] = $this->configuration->getCloud();
+            $settings['settings'] = $videoFreeParamsArray['player'];
+            $source = isset($videoFreeParamsArray['source']) ? $videoFreeParamsArray['source'] : null;
+            $settings['source'] = $source ?? [];
+        }
+
         if (empty($videoFreeParamsArray)) {
             $videoFreeParams = false;
         }
-        $videoFreeParamsArray['player']['cloudName'] = $this->configuration->getCloud();
-        $settings['settings'] = $videoFreeParamsArray['player'];
 
         // additional params
-        $source = $videoFreeParamsArray['source'] ?? null;
-        $settings['source'] = $source ?? [];
-
         if ($this->configuration->isEnabled() && $videoPlayerEnabled) {
             $settings['player_type'] = 'cloudinary';
             if (!$videoFreeParams) {
 
                 $transformation = [];
 
-                $autoplay = $allSettings['autoplay'];
-                $controls = $allSettings['controls'];
-                $isLoop = (bool) $allSettings['loop'];
+                $autoplay = isset($allSettings['autoplay']) ? $allSettings['autoplay'] : 'never';
+                $controls = isset($allSettings['controls']) ? $allSettings['controls'] : null;
+                $isLoop =  isset ($allSettings['loop']) ? (bool) $allSettings['loop'] : false;
                 $playerSettings = [
                     "cloudName" => $this->configuration->getCloud(),
                     'controls' => ($controls == 'all'),
@@ -66,20 +68,19 @@ class VideoSettings extends template
 
                 $playerSettings['muted'] = false;
 
-                $autoplayMode = $allSettings['autoplay'] ?? null;
-                if ($autoplayMode) {
-                    $playerSettings['autoplayMode'] = $autoplayMode;
-                    if ($autoplayMode != 'never') {
+                if ($autoplay) {
+                    $playerSettings['autoplayMode'] = $autoplay;
+                    if ($autoplay != 'never') {
                         $playerSettings['muted'] = true;
                     }
                 }
 
-                $streamMode = $allSettings['stream_mode'] ?? null;
+                $streamMode = isset($allSettings['stream_mode']) ? $allSettings['stream_mode'] : null;
 
                 if ($streamMode == 'optimization') {
-                    $streamModeFormat = $allSettings['stream_mode_format'] ?? null;
-                    $streamModeQuality = $allSettings['stream_mode_quality'] ?? null;
-                    $progressiveSourceTypes = $allSettings['progressive_sourcetypes'] ?? null;
+                    $streamModeFormat = isset($allSettings['stream_mode_format']) ? $allSettings['stream_mode_format'] :  null;
+                    $streamModeQuality = isset($allSettings['stream_mode_quality']) ? $allSettings['stream_mode_quality'] : null;
+                    $progressiveSourceTypes = isset($allSettings['progressive_sourcetypes']) ? $allSettings['progressive_sourcetypes'] : null;
 
                     if ($streamModeFormat == 'none' && $progressiveSourceTypes){
                         $sourceTypes = explode(',',$progressiveSourceTypes);
@@ -89,7 +90,7 @@ class VideoSettings extends template
                     }
                 }
                 if ($streamMode == 'abr') {
-                    $sourceTypes = $allSettings['source_types'] ? [$allSettings['source_types']] : null;
+                    $sourceTypes = isset($allSettings['source_types']) ? [$allSettings['source_types']] : null;
                 }
 
                 $settings = [
