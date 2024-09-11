c37e34d5c6e711b4ed8ec5aea9078da5e352e11a
Ariel Ashri <arieliens@gmail.com>
2024-09-11 19:29:48 +0300

    CLOUDINARY-522 - update cld media gallery with swatch images

--- view/frontend/web/js/swatch-renderer-mixin.js
+++ view/frontend/web/js/swatch-renderer-mixin.js
@@ -17,12 +17,16 @@ define([
                 publicId = publicId.replace('?_i=AB', '');
                 return {'publicId': publicId, 'mediaType': 'image'};
             },
+            mergeMediaAssets: function(existingAssets, newAssets) {
+                const existingPublicIds = existingAssets.map(asset => asset.publicId);
+                const uniqueNewAssets = newAssets.filter(newAsset => !existingPublicIds.includes(newAsset.publicId));
+                return [...existingAssets, ...uniqueNewAssets];
+            },
             _OnClick: function ($this, $widget) {
 
                 const loadedGallery = $('.cloudinary-product-gallery');
                 const cldPGid = loadedGallery.attr('id')
                 const cldGalleryWidget = window.cloudinary_pg[cldPGid] || null;
-
 
                 if (typeof this._super === 'function') {
                     this._super($this, $widget);
@@ -30,16 +34,11 @@ define([
 
                 const images = $widget.options.jsonConfig.images[$widget.getProduct()];
                 if (images && images.length > 0) {
-
-                    const mainImageUrl = images[0].url;
-                     const imgsToUpdate = images.map(image => this.extractImageName(image.img));
-                    //const imgsToUpdate = images.map(image => image.img);
+                    const imgsToUpdate = images.map(image => this.extractImageName(image.img));
 
                     if (cldGalleryWidget) {
-                        var loadedAssets = cldGalleryWidget.options.mediaAssets;
-                        var mediaAssets = [...loadedAssets, ...imgsToUpdate];
-                        console.log(mediaAssets);
-                        var selectedIndex = loadedAssets.length + 1;
+                        const loadedAssets = cldGalleryWidget.options.mediaAssets;
+                        const mediaAssets = this.mergeMediaAssets(loadedAssets, imgsToUpdate);
 
                         cldGalleryWidget.update({
                             mediaAssets: mediaAssets,
